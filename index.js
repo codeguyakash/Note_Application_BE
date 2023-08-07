bd803be78b08eb75b9f2253aecbd8e177d93e7c4
Akash <codeguyakash@gmail.com>
2023-08-07 11:22:21 +0530

    changing again

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
-const userRouter = require("./routes/UserRoutes");
-const noteRouter = require("./routes/noteRoutes");
+const userRouter = require("./src/routes/UserRoutes");
+const noteRouter = require("./src/routes/noteRoutes");
 const dotenv = require("dotenv");
 const cors = require("cors");
 
